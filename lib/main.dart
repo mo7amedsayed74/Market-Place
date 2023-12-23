6787bd27c168dbcf2edec9b69bebe908cc74b339
mo7amedsayed74 <mohammedsayed7414@gmail.com>
2023-12-23 21:01:25 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MarketPlace());
+}
+
+class MarketPlace extends StatelessWidget {
+  const MarketPlace({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
