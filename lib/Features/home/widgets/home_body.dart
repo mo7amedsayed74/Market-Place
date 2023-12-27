af18f4ec7ca0b72a837d43bda881f7e83746c9e5
EngHeba1 <eheba0302@gmail.com>
2023-12-27 17:17:36 +0200

    add nav bar to home screen

--- lib/Features/home/widgets/home_body.dart
+++ lib/Features/home/widgets/home_body.dart
@@ -7,10 +7,10 @@ class HomeBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold(
       bottomNavigationBar: BottomNavigationBar(
-        items: const [BottomNavigationBarItem(icon:  ImageIcon(AssetImage(""))),
-          BottomNavigationBarItem(icon: ImageIcon(AssetImage("") )),
-          BottomNavigationBarItem(icon: ImageIcon(AssetImage("") )),
-          BottomNavigationBarItem(icon: ImageIcon(AssetImage(""))),
+        items: const [BottomNavigationBarItem(icon:  ImageIcon(AssetImage("assets/images/home.png")),label: "Home"),
+          BottomNavigationBarItem(icon: ImageIcon(AssetImage("assets/images/car.png",) ),label: "Shop"),
+          BottomNavigationBarItem(icon: ImageIcon(AssetImage("assets/images/sale.png") ),label: "Sale"),
+          BottomNavigationBarItem(icon: ImageIcon(AssetImage("assets/images/Profile.png")),label: "Profile"),
               ],),
     );
   }
