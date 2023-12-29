b9ef611752981bfe26437dee3d52cf201a94fb0a
EngHeba1 <eheba0302@gmail.com>
2023-12-29 17:16:26 +0200

    listView item

--- lib/Features/home/presentation/views/shop_screen.dart
+++ lib/Features/home/presentation/views/shop_screen.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:market_placee/Core/utiles/app_color.dart';
+
+import '../../../../Core/widgets/custom_text_form_field.dart';
+import '../../widgets/custom_appbar.dart';
+import '../../widgets/custom_search_row.dart';
 
 class ShopScreen extends StatelessWidget {
   const ShopScreen({super.key});
 
+  // TextEditingController controller;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black,
+    return Column(
+      children: [
+        CustomAppBar(),
+        CustomSearchRow(),
+        SizedBox(
+          height: MediaQuery.of(context).size.height *.6,
+          child: ListView.builder(
+              itemBuilder: (context, index) {
+                return(Text("hi"));
+              },
+            itemCount: 5,
+          ),
+        )
+
+      ],
     );
   }
 }
---
+++ lib/Features/home/widgets/custom_search_row.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomSearchRow extends StatelessWidget {
+  const CustomSearchRow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Row(
+    children: [
+    Opacity(
+    opacity: .4,
+    child:
+    IconButton(onPressed: () {}, icon: Icon(Icons.arrow_back))),
+    Expanded(
+    child: TextField(
+    decoration: InputDecoration(
+    fillColor: Colors.black,
+    hintText: "What do you want to buy",
+    focusedBorder:OutlineInputBorder(
+    borderRadius: BorderRadius.circular(16),
+    borderSide: BorderSide(color: Colors.black54)) ,
+enabledBorder: OutlineInputBorder(
+borderRadius: BorderRadius.circular(16),
+borderSide: BorderSide(color: Colors.black12))),
+),
+)
+],
+),
+);
+  }
+}
